7222e2c34e8100ebbede2f9d41c32afed04faf85
liaukx <66898568+liaukx-tarc@users.noreply.github.com>
2022-04-13 13:45:24 +0800

    change collection save method

--- app/src/main/java/bigboss/team/learnpenguin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/bigboss/team/learnpenguin/ui/profile/ProfileFragment.kt
@@ -33,6 +33,7 @@ class ProfileFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var userDatabase: DatabaseReference
     private lateinit var courseDatabase: DatabaseReference
+    private lateinit var courseNameDatabase: DatabaseReference
     private lateinit var storageRef: StorageReference
 
     override fun onCreateView(
@@ -43,6 +44,7 @@ class ProfileFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         userDatabase = FirebaseDatabase.getInstance().getReference("User")
         courseDatabase = FirebaseDatabase.getInstance().getReference("Course")
+        courseNameDatabase = FirebaseDatabase.getInstance().getReference("CourseName")
         storageRef = FirebaseStorage.getInstance().reference
         auth = FirebaseAuth.getInstance()
 
@@ -52,69 +54,82 @@ class ProfileFragment : Fragment() {
         binding.fragmentContainerView.visibility = View.INVISIBLE
 
         userDatabase.child("User").child(auth.uid.toString()).get().addOnSuccessListener { userResult ->
-            courseDatabase.get().addOnSuccessListener { courseResult ->
-                //username
-                viewModel.username = userResult.child("username").value.toString()
-                binding.txtUsername.text = viewModel.username
+            courseNameDatabase.get().addOnSuccessListener { courseNameResult ->
+                courseDatabase.get().addOnSuccessListener { courseResult ->
+                    //username
+                    viewModel.username = userResult.child("username").value.toString()
+                    binding.txtUsername.text = viewModel.username
 
-                //role
-                viewModel.role = userResult.child("role").value.toString()
-                binding.txtRole.text = viewModel.role
+                    //role
+                    viewModel.role = userResult.child("role").value.toString()
+                    binding.txtRole.text = viewModel.role
 
-                for (i in 0 until userResult.child("collection").childrenCount) {
+                    for (i in 0 until userResult.child("collection").childrenCount) {
 
-                    val courseID = userResult.child("collection").child(i.toString()).value.toString()
-                    viewModel.collection.add(courseID)
+                        if(userResult.child("collection").child(i.toString()).value == true)
+                        {
+                            val courseID = courseNameResult.child(i.toString()).value.toString()
+                            viewModel.collection.add(courseID)
 
-                    //Vectical Linear Layout
-                    var linearLayout = LinearLayout(activity)
-                    var layoutParam: LinearLayout.LayoutParams = LinearLayout.LayoutParams(350, 450)
-                    layoutParam.setMargins(20, 30, 0, 20)
-                    linearLayout.layoutParams = layoutParam
-                    linearLayout.orientation = LinearLayout.VERTICAL
-                    linearLayout.setBackgroundColor(resources.getColor(R.color.gray))
-                    linearLayout.isClickable = true
-                    linearLayout.setOnClickListener {
-                        Navigation.findNavController(it).navigate(resources.getIdentifier(
-                            courseResult.child(courseID).child("id").value.toString(),
-                            "id", activity?.packageName))
+                            //Vectical Linear Layout
+                            var linearLayout = LinearLayout(activity)
+                            var layoutParam: LinearLayout.LayoutParams =
+                                LinearLayout.LayoutParams(350, 450)
+                            layoutParam.setMargins(20, 30, 0, 30)
+                            linearLayout.layoutParams = layoutParam
+                            linearLayout.orientation = LinearLayout.VERTICAL
+                            linearLayout.setBackgroundColor(resources.getColor(R.color.light_gray))
+                            linearLayout.isClickable = true
+                            linearLayout.setOnClickListener {
+                                Navigation.findNavController(it).navigate(
+                                    resources.getIdentifier(
+                                        courseResult.child(courseID).child("id").value.toString(),
+                                        "id", activity?.packageName
+                                    )
+                                )
+                            }
+
+                            //Image
+                            var imageView = ImageView(activity)
+                            var imgParam: LinearLayout.LayoutParams =
+                                LinearLayout.LayoutParams(350, 350)
+                            imageView.layoutParams = imgParam
+
+                            val file = File.createTempFile("temp", "png")
+
+                            storageRef.child("Image/${courseID}.png").getFile(file)
+                                .addOnSuccessListener {
+                                    val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                                    imageView.setImageBitmap(bitmap)
+                                }
+
+                                .addOnFailureListener { ex ->
+                                    imageView.setImageResource(R.drawable.penguin_logo)
+                                    toast(ex.message.toString())
+                                }
+
+                            linearLayout.addView(imageView)
+
+                            //Text
+                            var textView = TextView(activity)
+                            var txtParam: LinearLayout.LayoutParams =
+                                LinearLayout.LayoutParams(350, 100)
+                            txtParam.setMargins(0, 15, 0, 0)
+                            textView.layoutParams = txtParam
+                            textView.textSize = 16F
+                            textView.text = courseResult.child(courseID).child("name").value.toString()
+                            textView.ellipsize = TextUtils.TruncateAt.MARQUEE
+                            textView.maxLines = 1
+
+                            linearLayout.addView(textView)
+
+                            binding.linearCollection.addView(linearLayout)
+                        }
                     }
-
-                    //Image
-                    var imageView = ImageView(activity)
-                    var imgParam: LinearLayout.LayoutParams = LinearLayout.LayoutParams(350, 350)
-                    imageView.layoutParams = imgParam
-
-                    val file = File.createTempFile("temp", "png")
-                    val id = courseResult.child(courseID).child("language").value.toString()
-
-                    storageRef.child("Image/${id}.png").getFile(file)
-                        .addOnSuccessListener {
-                            val bitmap = BitmapFactory.decodeFile(file.absolutePath)
-                            imageView.setImageBitmap(bitmap)
-                        }
-
-                        .addOnFailureListener { ex ->
-                            imageView.setImageResource(R.drawable.penguin_logo)
-                            toast(ex.message.toString())
-                        }
-
-                    linearLayout.addView(imageView)
-
-                    //Text
-                    var textView = TextView(activity)
-                    var txtParam: LinearLayout.LayoutParams = LinearLayout.LayoutParams(350, 100)
-                    txtParam.setMargins(0, 15, 0, 0)
-                    textView.layoutParams = txtParam
-                    textView.textSize = 16F
-                    textView.text = courseResult.child(courseID).child("name").value.toString()
-                    textView.ellipsize = TextUtils.TruncateAt.MARQUEE
-                    textView.maxLines = 1
-
-                    linearLayout.addView(textView)
-
-                    binding.linearCollection.addView(linearLayout)
                 }
+                    .addOnFailureListener { ex ->
+                        toast(ex.message.toString())
+                    }
             }
                 .addOnFailureListener { ex ->
                     toast(ex.message.toString())
