3fb6f0a5b27ecad265cb564a960d8869e2d672ea
liaukx <66898568+liaukx-tarc@users.noreply.github.com>
2022-04-13 09:42:26 +0800

    Fix course menu crash problem

--- app/src/main/java/bigboss/team/learnpenguin/Adapter/CourseAdapter.kt
+++ app/src/main/java/bigboss/team/learnpenguin/Adapter/CourseAdapter.kt
@@ -1,36 +1,57 @@
 package bigboss.team.learnpenguin.Adapter
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import bigboss.team.learnpenguin.Interface.ItemClickListener
 import bigboss.team.learnpenguin.R
 import bigboss.team.learnpenguin.ui.course.CourseMenu
 import com.google.android.material.imageview.ShapeableImageView
 
-class CourseAdapter (private val courseList : ArrayList<CourseMenu>) : RecyclerView.Adapter<CourseAdapter.CourseViewHolder>() {
+class CourseViewHolder(courseView: View) : RecyclerView.ViewHolder(courseView), View.OnClickListener,View.OnLongClickListener {
 
-    private lateinit var itemListener: ItemClickListener
+    val courseImage: ShapeableImageView = courseView.findViewById(R.id.courseImage)
+    val courseHeading: TextView = courseView.findViewById(R.id.courseHeading)
 
-    interface ItemClickListener {
+    private var itemListener: ItemClickListener ?= null
 
-        fun onItemClick(position: Int)
-
+    init {
+        courseView.setOnClickListener(this)
     }
 
-    fun setOnClickListener(listener: ItemClickListener) {
-
-        itemListener = listener
-
+    fun setItemClickListener(itemClickListener: ItemClickListener)
+    {
+        this.itemListener = itemClickListener
     }
+
+
+    override fun onClick(v: View?) {
+        itemListener!!.onClick(v,absoluteAdapterPosition,false)
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        itemListener!!.onClick(v,absoluteAdapterPosition,true)
+        return true
+    }
+}
+
+class CourseAdapter (private val courseList : ArrayList<CourseMenu>, private val fragmentList: Array<Int>) : RecyclerView.Adapter<CourseViewHolder>() {
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
 
         val courseView =
             LayoutInflater.from(parent.context).inflate(R.layout.course_list, parent, false)
 
-        return CourseViewHolder(courseView, itemListener)
+        return CourseViewHolder(courseView)
     }
 
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
@@ -38,25 +59,20 @@ class CourseAdapter (private val courseList : ArrayList<CourseMenu>) : RecyclerV
         val currentItem = courseList[position]
         holder.courseImage.setImageBitmap(currentItem.image)
         holder.courseHeading.text = currentItem.heading
+
+        holder.setItemClickListener(ItemClickListener{ view, position, isLongClick ->
+
+            if(!isLongClick)
+            {
+                Navigation.findNavController(view).navigate(fragmentList[position])
+            }
+        } )
     }
 
     override fun getItemCount(): Int {
 
         return courseList.size
     }
-
-    class CourseViewHolder(courseView: View, listener: ItemClickListener) :
-        RecyclerView.ViewHolder(courseView) {
-
-        val courseImage: ShapeableImageView = courseView.findViewById(R.id.courseImage)
-        val courseHeading: TextView = courseView.findViewById(R.id.courseHeading)
-
-        init {
-            itemView.setOnClickListener {
-
-                listener.onItemClick(adapterPosition)
-
-            }
-        }
-    }
 }
+
+
